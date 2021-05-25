6657cb087ca25da1a408bbfa3ea8e91f177403c1
Dileep Kr. Chaudhary <mail@dileep.com.np>
2021-05-25 19:45:47 +0545

    First commit

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    public function index()
+    {
+        return view('contacts.index');
+    }
+
+    public function view()
+    {
+        return view('welcome');
+    }
+}
---
+++ resources/views/contacts/view.blade.php
@@ -0,0 +1,1 @@
+<h3>View Page</h3>
