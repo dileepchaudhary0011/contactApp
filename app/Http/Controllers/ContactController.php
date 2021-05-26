f0fe6671c0cdeb9a18a6fcbd76fc3b130fb1c60e
Dileep Kr. Chaudhary <mail@dileep.com.np>
2021-05-26 19:33:38 +0545

    Data passing example

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -8,7 +8,13 @@ class ContactController extends Controller
 {
     public function index()
     {
-        return view('contacts.index');
+        $string     =   "String value123";
+        $integerValue = 123;
+        $array  =   ['value 1', 'value 2', 'value 3'];
+        $keyArray   =   [array('name' => 'Dileep Kr. Chaudhary', 'address' => 'Gatthaghar'),
+                        array('name' => 'Dipesh', 'address' => 'Gatthaghar'),
+                        array('name' => 'Binod', 'address' => 'Gatthaghar')];
+        return view('contacts.index',compact('string', 'integerValue', 'array', 'keyArray'));
     }
 
     public function view()
--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -1,1 +1,22 @@
 <h3>Contact Page</h3>
+<strong>String Value</strong> : {{ $string }}
+{{ $integerValue }}
+<br/>
+
+<Strong>Array</Strong><br/>
+{{ var_dump($array) }}
+@foreach($array as $item)
+    {{ $item }}<br/>
+@endforeach
+
+<br/>
+<strong>For Loop</strong><br/>
+@for($i=0;$i<count($array);$i++)
+    {{ $array[$i] }}<br/>
+@endfor
+
+<br/>
+@foreach($keyArray as $item )
+    {{ $item['name'] }}<br/>
+    {{ $item['address'] }}<br/><br/>
+@endforeach
